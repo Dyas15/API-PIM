5da224fae4c69298bda14eeb7954e3a81268ba6e
Diogo <98923655+Dyas15@users.noreply.github.com>
2024-11-21 21:14:45 -0300

    Correção bug

--- routes/web.php
+++ routes/web.php
@@ -91,14 +91,14 @@ $router->group(['prefix' => '/api/produtos'], function () use ($router) {
 });
 
 $router->group(['prefix' => '/api/redefinicao'], function () use ($router) {
+    $router->get('/token-invalido', function () {
+        return view('emails.token_invalid');
+    });
     $router->get('/senha-alterada', function () {
         return view('emails.changed_password');
     });
     $router->get('/{token}', function ($token) {
         return view('emails.reset')->with(['token' => $token]);
-    });
-    $router->get('/token-invalido', function () {
-        return view('emails.token_invalid');
     });
     $router->post('/resetar', 'RedefinicaoController@ResetarSenha');
     $router->post('/enviar', 'RedefinicaoController@EnviarEmailDeReset');
